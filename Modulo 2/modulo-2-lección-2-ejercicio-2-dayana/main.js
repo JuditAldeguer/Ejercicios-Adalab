083992140703465411695620642d156379caf7c8
JuditAldeguer <jud.vicens@hotmail.es>
2021-08-05 20:12:24 +0200

    new ex.

--- Modulo 2/modulo-2-lección-2-ejercicio-2-dayana/main.js
+++ Modulo 2/modulo-2-lección-2-ejercicio-2-dayana/main.js
@@ -1,13 +1,6 @@
 'use strict';
 
-const button2 = document.querySelector ('.button2');
-button2.classList.add('disactivated')
+const superWave = document.querySelector('.super-wave'); // asignar
+console.log(superWave); // consultar valor en consola
+superWave.classList.add('hightlight'); // le añadimos clase nueva
 
-console.log(button2); //para que la Consola nos diga el valor de la variable / constante
-
-
-
-
-const welcomeParagraph = document.querySelector('.welcomeText'); //asigno variable
-console.log('welcomeParagraph: ', welcomeParagraph);// Logueo el contenido de la constante welcomeParagraph en la consola y compruebo que tiene asignado el elemento de HTML que espero antes de cambiar su contenido
-welcomeParagraph.innerHTML += ', Adalaber'; // modifico el texto de Html añadiendo Adalaber
