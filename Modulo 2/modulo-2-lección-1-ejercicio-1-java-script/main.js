7e379222073d17b4dbccae53b8dee55ddfdf3b4a
JuditAldeguer <jud.vicens@hotmail.es>
2021-08-05 10:51:59 +0200

    updated with shortcut

--- Modulo 2/modulo-2-lección-1-ejercicio-1-java-script/main.js
+++ Modulo 2/modulo-2-lección-1-ejercicio-1-java-script/main.js
@@ -1,14 +1,11 @@
 'use strict';
 
-let homeAdress = 'BCN'
-homeAdress = 'CST'
-
 // Obtenemos el elemento con el que queremos trabajar usando document.querySelector()
 const titleElement = document.querySelector('.title');
 // Cambiamos su contenido con innerHTML
-titleElement.innerHTML = 'Wellcome';
+titleElement.innerHTML = 'Welcome';
 // Cambiamos el contenido del elemento, indicando que sea igual al actual, más una nueva palabra añadida
-titleElement.innerHTML = titleElement.innerHTML + ' adalaber';
+titleElement.innerHTML += ', adalaber';
 
 
 const pSelection = document.querySelector ('.seleccion')
