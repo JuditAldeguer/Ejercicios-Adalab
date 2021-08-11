a2dc867e1f500c02f06bcc50c1373969edfa2069
JuditAldeguer <jud.vicens@hotmail.es>
2021-08-11 16:38:23 +0200

    ex

--- Modulo 2/modulo-2-lección-5-ejercicio-6-ej2event-currentTarget-classList.toggle/js/main.js
+++ Modulo 2/modulo-2-lección-5-ejercicio-6-ej2event-currentTarget-classList.toggle/js/main.js
@@ -7,14 +7,15 @@ const kiwi = document.querySelector(".fruit-kiwi");
 
 // handler
 function handleFruitClick(event) {
+  debugger;
   // Asignamos a una constante el elemento sobre el que pusimos el `listener` para trabajar cómodamente con él
   const selectedFruit = event.currentTarget;
-  console.log(event.currentTarget); //no hace nada----------------------
+  console.log(event.currentTarget);
   selectedFruit.classList.toggle("fruit--selected");
 }
 
 // listener sobre el elemento, con tipo de evento y handler
-debugger;
+
 strawberry.addEventListener("click", handleFruitClick);
 banana.addEventListener("click", handleFruitClick);
 kiwi.addEventListener("click", handleFruitClick);
