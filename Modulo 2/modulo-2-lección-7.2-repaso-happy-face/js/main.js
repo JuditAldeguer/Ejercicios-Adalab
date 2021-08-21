f331cf87d167e55d6614a2df3955d20c63aae407
JuditAldeguer <jud.vicens@hotmail.es>
2021-08-21 11:12:48 +0200

    console log Random num

--- Modulo 2/modulo-2-lección-7.2-repaso-happy-face/js/main.js
+++ Modulo 2/modulo-2-lección-7.2-repaso-happy-face/js/main.js
@@ -8,7 +8,6 @@ const body = document.querySelector('body');
 
 //Funcion
 function showResult(event) {
-  debugger;
   console.log(event.currentTarget);
   const clickedBtn = document.querySelector('.select');
   if (clickedBtn.value === 'sad') {
@@ -20,6 +19,30 @@ function showResult(event) {
     body.classList.add('happy');
     result.innerHTML = ':)';
   }
+
+  console.log(getRandomIntInclusive(1, 5));
+  console.log(getRandomInt(1, 5));
+  console.log(getRandomArbitrary(1, 5));
+  console.log(getRandom());
+}
+
+function getRandomIntInclusive(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1) + min); //The maximum is inclusive and the minimum is inclusive
+}
+
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min) + min); //The maximum is exclusive and the minimum is inclusive
+}
+
+function getRandomArbitrary(min, max) {
+  return Math.random() * (max - min) + min;
+}
+function getRandom() {
+  return Math.random();
 }
 
 //Listener
