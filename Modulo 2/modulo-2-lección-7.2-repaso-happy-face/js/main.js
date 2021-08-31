5321a80de1fbecf0cb01ce3f0e02868a3cbfff3c
JuditAldeguer <jud.vicens@hotmail.es>
2021-08-31 11:40:26 +0200

    info coment

--- Modulo 2/modulo-2-lección-7.2-repaso-happy-face/js/main.js
+++ Modulo 2/modulo-2-lección-7.2-repaso-happy-face/js/main.js
@@ -12,7 +12,7 @@ function showResult(event) {
   const clickedBtn = document.querySelector('.select');
   if (clickedBtn.value === 'sad') {
     body.classList.add('sad');
-    body.classList.add('happy');
+    body.classList.remove('happy');
     result.innerHTML = ':(';
   } else {
     body.classList.remove('sad');
@@ -29,20 +29,20 @@ function showResult(event) {
 function getRandomIntInclusive(min, max) {
   min = Math.ceil(min);
   max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min + 1) + min); //The maximum is inclusive and the minimum is inclusive
+  return Math.floor(Math.random() * (max - min + 1) + min); //The maximum is inclusive and the minimum is inclusive.
 }
 
 function getRandomInt(min, max) {
   min = Math.ceil(min);
   max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min) + min); //The maximum is exclusive and the minimum is inclusive
+  return Math.floor(Math.random() * (max - min) + min); //The maximum is exclusive and the minimum is inclusive.
 }
 
 function getRandomArbitrary(min, max) {
-  return Math.random() * (max - min) + min;
+  return Math.random() * (max - min) + min; //The maximum is exclusive and the minimum is inclusive.Puede contener decimales.
 }
 function getRandom() {
-  return Math.random();
+  return Math.random(); //The 1 is exclusive and the 0 is inclusive. Puede contener decimales.
 }
 
 //Listener
