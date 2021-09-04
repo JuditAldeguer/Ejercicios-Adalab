85ffe339a5ab327ba835bdc0f51585f12d3318ac
JuditAldeguer <jud.vicens@hotmail.es>
2021-09-05 00:20:56 +0200

    localStorage info

--- Modulo 2/modulo-2-lección-11-info -servidor2/js/main.js
+++ Modulo 2/modulo-2-lección-11-info -servidor2/js/main.js
@@ -1,1 +1,18 @@
 'use strict';
+
+//LocalStorage
+const tasks = [
+  { name: 'Recoger setas en el campo', completed: true },
+  { name: 'Comprar pilas', completed: true },
+  { name: 'Poner una lavadora de blancos', completed: true },
+  {
+    name: 'Aprender cómo funcionan los objetos de JavaScript',
+    completed: false,
+  },
+];
+
+localStorage.setItem('tasks', JSON.stringify(tasks)); // transforma JS en JSON
+console.log(localStorage.getItem('tasks')); // string - versión JSON
+
+const savedTasks = JSON.parse(localStorage.getItem('tasks')); // transforma JSON en JS
+console.log(savedTasks); //array - versión JS
