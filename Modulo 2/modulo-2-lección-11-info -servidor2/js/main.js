36caeb49d3f12fe635700dd21fd9793a5f0602cd
JuditAldeguer <jud.vicens@hotmail.es>
2021-09-06 09:35:45 +0200

    update ex 11 mistakes

--- Modulo 2/modulo-2-lección-11-info -servidor2/js/main.js
+++ Modulo 2/modulo-2-lección-11-info -servidor2/js/main.js
@@ -1,18 +1,30 @@
 'use strict';
 
-//LocalStorage
-const tasks = [
-  { name: 'Recoger setas en el campo', completed: true },
-  { name: 'Comprar pilas', completed: true },
-  { name: 'Poner una lavadora de blancos', completed: true },
-  {
-    name: 'Aprender cómo funcionan los objetos de JavaScript',
-    completed: false,
-  },
-];
+// //LocalStorage
+// const tasks = [
+//   { name: 'Recoger setas en el campo', completed: true },
+//   { name: 'Comprar pilas', completed: true },
+//   { name: 'Poner una lavadora de blancos', completed: true },
+//   {
+//     name: 'Aprender cómo funcionan los objetos de JavaScript',
+//     completed: false,
+//   },
+// ];
 
-localStorage.setItem('tasks', JSON.stringify(tasks)); // transforma JS en JSON
-console.log(localStorage.getItem('tasks')); // string - versión JSON
+// localStorage.setItem('tasks', JSON.stringify(tasks)); // transforma JS en JSON
+// console.log(localStorage.getItem('tasks')); // string - versión JSON
 
-const savedTasks = JSON.parse(localStorage.getItem('tasks')); // transforma JSON en JS
-console.log(savedTasks); //array - versión JS
+// const savedTasks = JSON.parse(localStorage.getItem('tasks')); // transforma JSON en JS
+// console.log(savedTasks); //array - versión JS
+
+//Errores al servidor
+fetch('https://api.github.com/repositories?since=abcds') //da error porque el parametro since es incorrecto, debe ser un número / fecha
+  .then((response) => {
+    debugger;
+    if (!response.ok) {
+      throw response;
+    }
+    return response.json();
+  })
+  .then((data) => console.log('success', data))
+  .catch((err) => console.log('error', err));
