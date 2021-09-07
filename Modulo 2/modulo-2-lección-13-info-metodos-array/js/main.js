881c3a149082e5d4b6c73cb5527a8065b5d037e6
JuditAldeguer <jud.vicens@hotmail.es>
2021-09-07 17:18:24 +0200

    media ex 7

--- Modulo 2/modulo-2-lección-13-info-metodos-array/js/main.js
+++ Modulo 2/modulo-2-lección-13-info-metodos-array/js/main.js
@@ -12,7 +12,14 @@ const var1 = ['Fire', 'Air', 'Water'];
 // const capitalVar1 = var1.map((var1) => var1.toUpperCase());
 // console.log(capitalVar1);
 
-//filter() filtra
-const names = ['María', 'Lucía', 'Susana', 'Rocío', 'Inmaculada'];
-const longNames = names.filter((name) => name.length > 5);
-console.log(`Los nombres con más de 5 letras son ${longNames}`); // Susana Inmaculada
+// //filter() filtra
+// const names = ['María', 'Lucía', 'Susana', 'Rocío', 'Inmaculada'];
+// const longNames = names.filter((name) => name.length > 5);
+// console.log(`Los nombres con más de 5 letras son ${longNames}`); // Susana Inmaculada
+
+//reduce()
+const scores = [4, 2, 7, 8, 6, 7, 9, 1, 2, 6, 7];
+const result = scores.reduce((acc, number) => acc + number, 0); // acc es una funcion con parametro de la funcion "acumulador de resultados" que se llama acc y es =0 inicialmente pero va obteniendo la suma de los números en él acc=acc+number[i]. Mientras que number es un parámetro con el valor variable number=number[i].value.
+console.log(result);
+const result2 = scores.reduce((acc, number) => acc + number); //el valor del acomulador inicial 0, es opcional. Sino lo inidcamos, tomara el number[0] como valor. En este caso, no influye.
+console.log(result2);
