a7124462eceb3ae0bf774c62e926c47f811b203c
JuditAldeguer <jud.vicens@hotmail.es>
2021-09-17 09:02:04 +0200

    update

--- Modulo 3/react-starter-kit/src/components/App.js
+++ Modulo 3/react-starter-kit/src/components/App.js
@@ -1,9 +1,13 @@
-import '../styles/App.css';
+import '../styles/App.scss';
+import happyFace from '../images/happyFace.jpeg';
 
 function App() {
   return (
     <div>
-      <h1>Hola mundo</h1>
+      <header>
+        <h1 className="title">Título de mi página</h1>
+      </header>
+      <img src="happyFace" alt="Happy face"></img>
     </div>
   );
 }
