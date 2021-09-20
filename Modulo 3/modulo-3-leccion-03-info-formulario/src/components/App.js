9f902a55711707a4fc972cd741b74250c8045881
JuditAldeguer <jud.vicens@hotmail.es>
2021-09-20 17:02:42 +0200

    update react starter kit

---
+++ Modulo 3/modulo-3-leccion-03-info-formulario/src/components/App.js
@@ -0,0 +1,13 @@
+import '../styles/App.scss';
+
+function App() {
+  return (
+    <div>
+      <header>
+        <h1 className="title">Formularios en React</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
