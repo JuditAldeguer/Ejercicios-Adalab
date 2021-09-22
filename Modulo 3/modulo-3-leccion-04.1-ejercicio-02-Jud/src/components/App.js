c43c7ee1ce0192770847a2361a30b3c1d4eb2e47
JuditAldeguer <jud.vicens@hotmail.es>
2021-09-22 13:22:18 +0200

    update with mistakes

--- Modulo 3/modulo-3-leccion-04.1-ejercicio-02-Jud/src/components/App.js
+++ Modulo 3/modulo-3-leccion-04.1-ejercicio-02-Jud/src/components/App.js
@@ -3,31 +3,60 @@ import { useState } from 'react';
 import tasksData from '../data.json'; //importa de otro file
 
 function App() {
+  //STATES----------------------------------------------------------
   const [tasks, setTasks] = useState(tasksData); //mueve datos de file a state
+  const [searchTask, setSearchTask] = useState('');
+  const [numList, setNumList] = useState(0);
+  const [numCompleted, setNumCompleted] = useState(0);
+
+  //FUNCTION---------------------------------------------------------
   const handleClick = (ev) => {
-    console.log(ev.currentTarget);
     const clickedEl = tasks.find(
       (task) => task.id === parseInt(ev.currentTarget.id)
     );
-    console.log(clickedEl);
     clickedEl.completed = !clickedEl.completed;
     setTasks([...tasks]); //se actualizan datos en el use state
   };
-  const renderTasks = () => {
-    return tasks.map((taskEl, i) => {
-      taskEl.id = i; //añado id a State
-      return (
-        <li
-          key={i}
-          id={i}
-          onClick={handleClick}
-          className={taskEl.completed === true ? 'done' : ''}
-        >
-          {taskEl.task}
-        </li>
-      );
-    });
+  const handleFilter = (ev) => {
+    setSearchTask(ev.currentTarget.value);
   };
+  const renderTasks = () => {
+    return (
+      tasks
+        // Filtramos por nombre
+        .filter((taskEl) => {
+          taskEl.id = taskEl.id + '0'; //apaño para modificar id en State, cambia de orden al filtrar
+          return taskEl.task.toLowerCase().includes(searchTask.toLowerCase());
+        })
+        // Mapeamos
+        .map((taskEl, i) => {
+          taskEl.id = i; //añado id a State
+          return (
+            <li
+              key={i}
+              id={i}
+              onClick={handleClick}
+              className={taskEl.completed === true ? 'done' : ''}
+            >
+              {taskEl.task}
+            </li>
+          );
+        })
+    );
+  };
+
+  // //da error---------------
+  const handleNumCompleted = () => {
+    //donde llamo a esta función???????????????????????????????????????
+    for (let i = 0; i < tasks.length; i++) {
+      setNumList(i + 1); //añadir num li a State
+    }
+    const completedTask = tasks.find((taskEl) => taskEl.completed === true);
+    const completedNum = completedTask.key + 1;
+    setNumCompleted(completedNum);
+  };
+
+  //HTML-----------------------------------------------------------------------
   return (
     <div>
       <header>
@@ -35,6 +64,19 @@ function App() {
       </header>
       <main>
         <ul>{renderTasks()}</ul>
+        <label htmlFor="filter"></label>
+        <input
+          onChange={handleFilter}
+          type="text"
+          name="filter"
+          id="filter"
+          value={searchTask}
+        />
+        <ul>
+          <li>Tareas totales: {numList}</li>
+          <li>Tareas completadas: 2 {/* {numCompleted} */}</li>
+          <li>Tareas pendientes: 3 {/* {numList - numCompleted}*/}</li>
+        </ul>
       </main>
     </div>
   );
